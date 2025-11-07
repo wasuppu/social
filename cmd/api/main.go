a521bab0cb00142efd5ac5d9875ea113e643cc0f
lundrore <101697913+lundrore@users.noreply.github.com>
2025-11-07 21:45:35 +0800

    feat: hot reloading and environment variables

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,13 @@
 package main
 
-import "log"
+import (
+	"log"
+	"social/internal/env"
+)
 
 func main() {
 	cfg := config{
-		addr: ":8080",
+		addr: env.GetString("ADDR", ":8080"),
 	}
 
 	app := &application{
