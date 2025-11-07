cef14b9006ded1cda22822a1f0aa5f0f0a6036cd
lundrore <101697913+lundrore@users.noreply.github.com>
2025-11-07 16:53:05 +0800

    feat: clean layered architecture

---
+++ cmd/api/api.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type config struct {
+	addr string
+}
+
+type application struct {
+	config config
+}
+
+func (app *application) mount() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /v1/health", app.healthCheckHandler)
+
+	return mux
+}
+
+func (app *application) run(mux *http.ServeMux) error {
+	srv := &http.Server{
+		Addr:         app.config.addr,
+		Handler:      mux,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 10,
+		IdleTimeout:  time.Minute,
+	}
+
+	log.Printf("server has started at %s", app.config.addr)
+
+	return srv.ListenAndServe()
+}
---
+++ cmd/api/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "log"
+
+func main() {
+	cfg := config{
+		addr: ":8080",
+	}
+
+	app := &application{
+		config: cfg,
+	}
+
+	mux := app.mount()
+
+	log.Fatal(app.run(mux))
+}
