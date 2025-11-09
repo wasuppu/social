acc0a8be95742f77f7aa7a83d8766e18ff6a1d65
lundrore <101697913+lundrore@users.noreply.github.com>
2025-11-09 14:38:05 +0800

    feat: http payload validation

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -10,14 +10,24 @@ import (
 )
 
 type CreatePostPayload struct {
-	Title   string   `json:"title"`
-	Content string   `json:"content"`
+	Title   string   `json:"title" validate:"required,max=100"`
+	Content string   `json:"content" validate:"required,max=1000"`
 	Tags    []string `json:"tags"`
 }
 
 func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request) {
 	var payload CreatePostPayload
 	if err := readJSON(w, r, &payload); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	// if payload.Content == "" {
+	// 	app.badRequestResponse(w, r, fmt.Errorf("content is required"))
+	// 	return
+	// }
+
+	if err := Validate.Struct(payload); err != nil {
 		app.badRequestResponse(w, r, err)
 		return
 	}
--- go.mod
+++ go.mod
@@ -1,8 +1,19 @@
 module social
 
-go 1.24
+go 1.24.0
 
 require (
-    github.com/go-chi/chi/v5 v5.2.3
-    github.com/lib/pq v1.10.9
+	github.com/go-chi/chi/v5 v5.2.3
+	github.com/lib/pq v1.10.9
+	github.com/go-playground/validator/v10 v10.28.0
+)
+
+require (
+	github.com/gabriel-vasile/mimetype v1.4.11 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	golang.org/x/crypto v0.43.0 // indirect
+	golang.org/x/sys v0.38.0 // indirect
+	golang.org/x/text v0.30.0 // indirect
 )
