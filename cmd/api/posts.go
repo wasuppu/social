e1f5e508a411fd884d73af3caff6b74dabcc09c8
lundrore <101697913+lundrore@users.noreply.github.com>
2025-11-09 14:07:47 +0800

    feat: getting a post from a user

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 	"social/internal/store"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
 )
 
 type CreatePostPayload struct {
@@ -34,6 +38,33 @@ func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request
 	}
 
 	if err := app.jsonResponse(w, http.StatusCreated, post); err != nil {
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+}
+
+func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request) {
+	idParam := chi.URLParam(r, "postID")
+	id, err := strconv.ParseInt(idParam, 10, 64)
+	if err != nil {
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	ctx := r.Context()
+
+	post, err := app.store.Posts.GetByID(ctx, id)
+	if err != nil {
+		switch {
+		case errors.Is(err, store.ErrNotFound):
+			writeJSONError(w, http.StatusNotFound, err.Error())
+		default:
+			writeJSONError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+
+	if err := app.jsonResponse(w, http.StatusOK, post); err != nil {
 		writeJSONError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
--- internal/store/storage.go
+++ internal/store/storage.go
@@ -3,10 +3,16 @@ package store
 import (
 	"context"
 	"database/sql"
+	"errors"
+)
+
+var (
+	ErrNotFound = errors.New("resource not found")
 )
 
 type Storage struct {
 	Posts interface {
+		GetByID(context.Context, int64) (*Post, error)
 		Create(context.Context, *Post) error
 	}
 
